6957c87b4f90d7f57d86c55caa95055ca949a640
Aly <aschmidt36@my.wctc.edu>
2016-01-29 16:48:13 -0600

    added getCircleArea and getTriangleArea to service

--- src/java/model/AreaCalculationService.java
+++ src/java/model/AreaCalculationService.java
@@ -5,7 +5,8 @@ package model;
  * @author Alyson
  */
 public class AreaCalculationService {
-
+    private String area;
+    private double a;
     /**
      * Calculates the area of a rectangle by passing in height and width.
      * height and width are converted to double and then used to calculate
@@ -16,17 +17,34 @@ public class AreaCalculationService {
      * @return area of rectangle 
      */
     public String getRectangleArea(String length, String width) throws IllegalArgumentException{
-        String area;
-        
         double l = Double.parseDouble(length);
         double w = Double.parseDouble(width);
         
-        double a = l * w;
+        a = l * w;
         
         area = "" + a;
         
         return area;
     }
     
-    public String getCircleArea(String radius)
+    public String getCircleArea(String radius) throws IllegalArgumentException{
+        double r = Double.parseDouble(radius);
+        
+        a = Math.PI*(Math.pow(r, 2));
+        
+        area = "" + a;
+        
+        return area;
+    }
+    
+    public String getTriangleArea(String base, String height) throws IllegalArgumentException{
+        double b = Double.parseDouble(base);
+        double h = Double.parseDouble(height);
+        
+        a = (0.5 * b * h);
+        
+        area = "" + a;
+        
+        return area;
+    }
 }
